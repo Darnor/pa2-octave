9931c2dffa446f4ca0efe2dc1338609a0bddd4db
Hansruedi Patzen <hp@revy.ch>
2019-12-05 00:14:45 +0100

    code: update series

--- tests/series.m
+++ tests/series.m
@@ -1,21 +1,64 @@
-function G = image_segmentation_series(N_limit)
+function image_segmentation_fixed_graph_series(C, l, b)
+  [chi, lambda] = generate_fixed_graph(l, b);
+endfunction
+
+function image_segmentation_series(C, N_limit)
+  # 
+endfunction
+
+function difference_series(C, N_limit)
   
 endfunction
 
-function G = differenze_series(N_limit)
+function gradient_series(C, N_limit)
   
 endfunction
 
-function R = gradient_series(N_limit)
-  
+function [chi_r, lambda_r] = generate_fixed_graph(l, b)
+  # Use persistent variables to keep to not recreate them for every test.
+  persistent chi;
+  persistent lambda;
+  persistent L8P;
+
+  if (isempty(L8P))
+    L8P = laplace_eight_point(l, b);
+  endif
+
+  if (isempty(chi) || isempty(lambda))
+    [chi, lambda] = eig(L8P, "vector");
+  endif
+
+  chi_r = chi;
+  lambda_r = lambda;
+endfunction
+
+function run_all_test_series_for(C)
+  N_limit = 1000;
+  l = 40; b = 25; # to keep consistent with N_limit = 1000.
+
+  disp("Starting segmentation with fixed graph test");
+  image_segmentation_fixed_graph_series(C, l, b);
+
+  disp("Starting segmentation test");
+  image_segmentation_series(C, N_limit);
+
+  disp("Starting difference test");
+  difference_series(C, N_limit);
+
+  disp("Starting gradient test");
+  gradient_series(C, N_limit);
 endfunction
 
 function run_moon_test_series_for(impath)
-  C = read_all_images(impath);
+  disp("Starting moon tests");
+  run_all_test_series_for(read_all_images(impath));
+  disp("Moon series done");
 endfunction
 
 function run_sun_test_series_for(impath)
-  C = read_all_images_raw(impath);
+  disp("Starting sun tests");
+  run_all_test_series_for(read_all_images_raw(impath));
+  disp("Sun series done");
 endfunction
 
 function run_sun_series(impath)
@@ -29,10 +72,10 @@ endfunction
 
 function run_moon_series(impath)
   run_moon_test_series_for([impath "/moon-1/"]);
-  run_moon_test_series_for([impath "/moon-2/"]);
-  run_moon_test_series_for([impath "/moon-3/"]);
-  run_moon_test_series_for([impath "/moon-4/"]);
-  run_moon_test_series_for([impath "/moon-5/"]);
+  #run_moon_test_series_for([impath "/moon-2/"]);
+  #run_moon_test_series_for([impath "/moon-3/"]);
+  #run_moon_test_series_for([impath "/moon-4/"]);
+  #run_moon_test_series_for([impath "/moon-5/"]);
 endfunction
 
 function run_all_moon_series()
